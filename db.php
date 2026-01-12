47d3e038a7a52c10135b0b2bf1bbddb7be9fb9a2
dy2328-ctrl <dy2328@keanm.cloud>
2026-01-12 17:23:23 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
+$host = 'db5019378605.hosting-data.io ';
+$db   = 'dbs15162823'; 
+$user = 'dbu2244961';
+$pass = 'kuqteg-ginbak-myKga7';
 
-$host = 'db5019378605.hosting-data.io'; 
-$db   = 'dbs15162823';  
-$user = 'dbu2244961';                 
-$pass = 'YOUR_PASSWORD';         
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
